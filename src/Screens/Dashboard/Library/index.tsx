c263ecefab6a37357b02adace7ad25e69a06832e
Emmanuel <emmanuelademola20@gmail.com>
2024-03-30 18:40:20 +0100

    created library screen, tabs view

--- src/Screens/Dashboard/Library/index.tsx
+++ src/Screens/Dashboard/Library/index.tsx
@@ -1,11 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { FlatList, ListRenderItem } from 'react-native'
+import ScreenTitleView from '@reusables/ScreenTitleView'
+import Screen from '@Utilities/Screen'
+import Wrapper from '@Utilities/Wrapper'
+import UITabs from '@UILibrary/UITabs'
+import UIView from '@UILibrary/UIView'
+import { SUBSCRIPTIONS_LIST } from '@data/index'
+import { SubscriptionListPropTypes } from '@data/interface'
+import useTabs from '@hooks/useTabs'
+import SubscriptionTile from '../AllSubscriptions/components/SubscriptionTile'
+
+const TABS = ['Subscriptions', 'Queue', 'Downloads']
 
 const LibraryScreen = () => {
+  const { activeTab, onTabSwitch } = useTabs(TABS)
+  const renderSubscriptions: ListRenderItem<SubscriptionListPropTypes> = ({
+    item,
+  }) => <SubscriptionTile {...item} />
+
   return (
-    <View>
-      <Text>LibraryScreen</Text>
-    </View>
+    <Screen>
+      <ScreenTitleView title='Library' withMicIcon />
+      <Wrapper flex={1}>
+        <UITabs tabs={TABS} activeTab={activeTab} onTabSwitch={onTabSwitch} />
+        <UIView my={30}>
+          <FlatList
+            data={SUBSCRIPTIONS_LIST}
+            renderItem={renderSubscriptions}
+          />
+        </UIView>
+      </Wrapper>
+    </Screen>
   )
 }
 
